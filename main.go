e3b010c56a8c20bac0b02a222836fbc18edc9b6e
dsw <danangsukma@gmail.com>
2019-08-21 22:19:09 +0700

    update message and main function

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"flag"
+	"fmt"
+	"os"
+	samples "github.com/danangsw/goexamples/samples"
+)
+
+const hello = "hello"
+const helloNameFlag = "name"
+const value = "values"
+const defaultMsg = "let's go! learn golang by examples."
 
 func main () {
-	fmt.Println("let's go! learn golang by examples.")
+	helloCmd := flag.NewFlagSet(hello, flag.ExitOnError)
+	helloName := helloCmd.String(helloNameFlag, "", "hello -name=<Your name here>")
+
+	valueCmd := flag.NewFlagSet(value, flag.ExitOnError)
+
+	if len(os.Args) < 2 {
+		fmt.Println(defaultMsg)
+		os.Exit(1)	
+	}
+
+	switch os.Args[1] {
+		case hello:
+			helloCmd.Parse(os.Args[2:])
+			samples.Hello(*helloName)
+		case value:
+			valueCmd.Parse(os.Args[2:])
+			samples.Values()
+		default:
+			fmt.Println(defaultMsg)
+			os.Exit(1)
+	}
 }
