ca79572be64812e4766c5b18e16db69988f2487b
dsw <danangsukma@gmail.com>
2019-08-22 10:11:04 +0700

    add reverse testing

---
+++ testing/reverse_test.go
@@ -0,0 +1,22 @@
+package testing_test
+
+import (
+	"testing"
+	samples "github.com/danangsw/goexamples/samples"
+)
+
+func TestReverse(t *testing.T)  {
+	cases := []struct {
+		in, want string
+	} {
+		{"Hello, world", "dlrow ,olleH"},
+		{"Hello, 世界", "界世 ,olleH"},
+		{"", ""},
+	}
+	for _, c := range cases {
+		got := samples.Reverse(c.in)
+		if got != c.want {
+			t.Errorf("Reverse(%q) == %q, want %q", c.in, got, c.want)
+		}
+	}
+}
