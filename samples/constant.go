51a8997179e0c763a7178735bb1cedaa4f2f5d2f
Danang Sukma Wantara <danangsukma@gmail.com>
2025-08-28 11:10:08 +0700

    add constant samples

---
+++ samples/constant.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+const Pi = 3.14159
+const Greeting = "Hello, Go!"
+const n = 5000000
+const d = 3e20 / 5
+
+func main() {
+	fmt.Println("Pi:", Pi)
+	fmt.Println(Greeting)
+	fmt.Println("n:", n)
+	fmt.Println("d:", d)
+	// Performing calculations
+	fmt.Println("Square root of n: ", math.Sqrt(n))
+	fmt.Println("Sin of n:", math.Sin((n)));
+}
