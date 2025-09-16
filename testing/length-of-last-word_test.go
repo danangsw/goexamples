6c5715ad283cfe3f0e9a4e1811105a104aa2a3a2
danang sukma wantara <danangsukma@gmail.com>
2025-09-16 11:32:18 +0700

    Update Length of last word problem unit tests

--- testing/length-of-last-word_test.go
+++ testing/length-of-last-word_test.go
@@ -2,10 +2,11 @@ package testing_test
 
 import (
 	"testing"
+
 	problem "../problem"
 )
 
-func TestLengthOfLastWord1(t *testing.T) {
+func TestLengthOfLastWord(t *testing.T) {
 	testCases := []struct {
 		s      string
 		expect int
@@ -30,27 +31,6 @@ func TestLengthOfLastWord1(t *testing.T) {
 		if result != tc.expect {
 			t.Errorf("LengthOfLastWord1: For s=%q, expected %v, but got %v", tc.s, tc.expect, result)
 		}
-	}
-}
-
-func TestLengthOfLastWord2(t *testing.T) {
-	testCases := []struct {
-		s      string
-		expect int
-	}{
-		{s: "Hello World", expect: 5},
-		{s: "   fly me   to   the moon  ", expect: 4},
-		{s: "luffy is still joyboy", expect: 6},
-		{s: "word", expect: 4},
-		{s: " word", expect: 4},
-		{s: "word ", expect: 4},
-		{s: " word ", expect: 4},
-		{s: "a", expect: 1},
-		{s: " a", expect: 1},
-		{s: "a ", expect: 1},
-		{s: " a ", expect: 1},
-		{s: "  a  ", expect: 1},
-		{s: " b   a    ", expect: 1},
 	}
 
 	for _, tc := range testCases {
