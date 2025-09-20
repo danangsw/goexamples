fcfa2c76d81349d1095fb4ba74ca0a2aed5351ec
danang sukma wantara <danangsukma@gmail.com>
2025-09-20 23:39:22 +0700

    Refactor for readibility

--- problem/same-tree.go
+++ problem/same-tree.go
@@ -12,46 +12,30 @@ package problem
 func IsSameTree(p *TreeNode, q *TreeNode) bool {
 	sp, sq := []*TreeNode{}, []*TreeNode{}
 
-	if q == p {
-		return true
-	}
-	if q == nil || p == nil {
-		return false
-	}
-
 	sp = append(sp, p)
 	sq = append(sq, q)
 
 	for len(sp) > 0 && len(sq) > 0 {
 		cp := sp[len(sp)-1]
-		sp = sp[:len(sp)-1]
-
 		cq := sq[len(sq)-1]
+
+		sp = sp[:len(sp)-1]
 		sq = sq[:len(sq)-1]
 
+		if cp == nil && cq == nil {
+			continue
+		}
+		if cp == nil || cq == nil {
+			return false
+		}
 		if cp.Val != cq.Val {
 			return false
 		}
 
-		if cp.Right != nil {
-			sp = append(sp, cp.Right)
-		}
-		if cq.Right != nil {
-			sq = append(sq, cq.Right)
-		}
-		if len(sq) != len(sp) {
-			return false
-		}
-
-		if cp.Left != nil {
-			sp = append(sp, cp.Left)
-		}
-		if cq.Left != nil {
-			sq = append(sq, cq.Left)
-		}
-		if len(sq) != len(sp) {
-			return false
-		}
+		sp = append(sp, cp.Right)
+		sp = append(sp, cp.Left)
+		sq = append(sq, cq.Right)
+		sq = append(sq, cq.Left)
 	}
 
 	return true
