12285baca6149d6489493c0a14d13301578d7ae2
danang sukma wantara <danangsukma@gmail.com>
2025-09-20 14:15:21 +0700

    Setup global setting in testing package

--- testing/global_setting_test.go
+++ testing/global_setting_test.go
@@ -9,3 +9,9 @@ type TreeNode = helper.TreeNode
 
 // You can add global settings or configurations for your tests here.
 // For example, you might want to set up logging, initialize databases, etc.
+func init() {
+	// Example: Set up a global logger
+	// log.SetOutput(os.Stdout)
+	// log.SetLevel(log.DebugLevel)
+	// log.Info("Global test settings initialized")
+}
