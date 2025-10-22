d2e2751a3c975529e318b1c1f4aeed17384a702d
danang sukma wantara <danangsukma@gmail.com>
2025-10-22 18:03:58 +0700

    Add count bits solution: Dynamic Programming

--- problem/counting-bits.go
+++ problem/counting-bits.go
@@ -2,6 +2,7 @@ package problem
 
 // https://leetcode.com/problems/counting-bits/
 
+// Shift-based approach
 func CountBits(n int) []int {
 	result := make([]int, n+1)
 	for i := 1; i <= n; i++ {
@@ -17,6 +18,30 @@ func CountBits(n int) []int {
 		// i=5: ans[5]=ans[2]+1=2
 		// Result: [0,1,1,2,1,2]
 		result[i] = result[i>>1] + (i & 1)
+	}
+	return result
+}
+
+// Dynamic Programming approach
+func CountBitsDP(n int) []int {
+	result := make([]int, n+1)
+	// offset tracks the largest power of 2 less than or equal to i
+	offset := 1
+	for i := 1; i <= n; i++ {
+		// When i reaches the next power of 2, update offset
+		if i == offset*2 {
+			offset *= 2
+		}
+		// The number of 1's in i is 1 (for the leading 1) plus
+		// the number of 1's in the remainder (i - offset)
+		// Example trace (DP-based for n=5):
+		// i=1: ans[1]=ans[0]+1=1
+		// i=2: ans[2]=ans[0]+1=1
+		// i=3: ans[3]=ans[1]+1=2
+		// i=4: ans[4]=ans[0]+1=1
+		// i=5: ans[5]=ans[1]+1=2
+		// Result: [0,1,1,2,1,2]
+		result[i] = result[i-offset] + 1
 	}
 	return result
 }
