4d6c0499cc79cbbb1024307e8abc8c3e2a1de631
danang sukma wantara <danangsukma@gmail.com>
2025-10-23 20:48:01 +0700

    Add Reverse bit problem

---
+++ problem/reverse-bits.go
@@ -0,0 +1,36 @@
+package problem
+
+// https://leetcode.com/problems/reverse-bits/
+
+// pre computed table for reversed 8-bit value
+var table [256]int
+
+// using table[0..255] to store reversed bits of 0..255
+// where table[i] = reverseBits(i)
+func init() {
+	for i := range 256 {
+		table[i] = reverse8Bits(int(i))
+	}
+}
+
+// ReverseBits reverses the bits of a given 32 bits unsigned integer.
+// Time Complexity: O(1) - constant time due to fixed number of operations
+// Space Complexity: O(1) - constant space for the lookup table	
+func ReverseBits(n int) int {
+	b0 := int(n & 0xff)
+    b1 := int((n >> 8) & 0xff)
+    b2 := int((n >> 16) & 0xff)
+    b3 := int((n >> 24) & 0xff)
+
+    return (table[b0] << 24) | (table[b1] << 16) | (table[b2] << 8) | table[b3]
+}
+
+// reverse8Bits reverses the bits of an 8-bit integer.
+func reverse8Bits(b int) int {
+	var rev int = 0
+	for range 8 {
+		rev = (rev << 1) | (b & 1)
+		b >>= 1
+	}
+	return rev
+}
